c0243f7d113d0af9a0fb901897b5cbfdea1a9a1c
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-10-16 12:10:01 -0600

    Setup the spawnBall
    I started to utilize the object notation, to create a ball  on the screeno n command, now i start utilizing gravity to affect how the balla cts and animates as this will be a huge part of my final project as I plan to influence this trajectory system into a 3d world so it has to be efficient

--- 07-ball-trajectory/sketch.js
+++ 07-ball-trajectory/sketch.js
@@ -5,13 +5,55 @@
 // Extra for Experts:
 // Learned to go above and beyond just arrays and used constructors and specified the bullet its own properties
 
+const dx = 12;
 
+let dy;
+let x;
+let y;
+let shoot;
+let enter;
+let t;
+let bullet;
+let bulletArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/3;
+  y = height-height/3;
+  dy = -25;
+  shoot = false;
+  enter = 13;
+  t = millis();
+  spawnBullet();
 }
 
 function draw() {
   background(220);
+
+  if (keyIsDown(enter)){
+    shoot = true;
+  }
+
+  if (millis() - t >= 2000){
+    shoot = false;
+    t = millis();
+  }
+
+  if (shoot){
+    for (let theBullet of bulletArray){
+      theBullet.x = x;
+      theBullet.y = y;
+      circle(theBullet.x, theBullet.y, 50);
+    }
+    console.log(shoot);
+  }
+  console.log(shoot);
 }
 
+function spawnBullet(){
+  let bullet = {
+    bx: x,
+    by: y,
+  };
+  bulletArray.push(bullet);
+}
