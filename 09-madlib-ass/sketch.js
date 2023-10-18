2bd9157337b3cd6fdeb2eb04097fe8194ca7a894
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-10-18 12:18:53 -0600

    Started on my madlibs
    I started to learn how to take user input and translate into something i can do with madlibs in it

--- 09-madlib-ass/sketch.js
+++ 09-madlib-ass/sketch.js
@@ -1,15 +1,45 @@
-// Project Title
-// Your Name
-// Date
+// Madlibs using Object Notation
+// Hyder Shahzaib Ahmed
+// 10/18/2023
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// My extra for experts would be using the premade button and inout field functions of P5js, the only reason I used the premade ones were to keep the code simplistic.
 
+
+let verbInput;
+let submitButton;
+let userInput;
+let verb;
+let pick_verb = "something";
+let madlibs = [
+  `Anna was visiting Grandmas house and decided to ${pick_verb} in the attic while grandma ${pick_verb} lunch. There were`
+];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // Create an input field
+  verbInput = createInput();
+  verbInput.position(20, 20);
+
+  // Create a submit button
+  submitButton = createButton("Submit");
+  submitButton.position(20, 50);
+  submitButton.mousePressed(processInput);
+  console.log(madlibs[0]);
 }
 
 function draw() {
   background(220);
+
+  console.log(verb);
+}
+
+function processInput() {
+  // Get the value entered by the user in the input field
+  verb = verbInput.value();
+  // Display the user's input
+  textSize(42);
+  text(`You entered: ${userInput}`, 20, 100);
+  // You can perform further actions with the user's input here
 }
