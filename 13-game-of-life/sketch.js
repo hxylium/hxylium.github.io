bae219d16f5945043584ad1288ca462fd400d6ba
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-10-26 12:19:12 -0600

    corrected the game of life code

--- 13-game-of-life/sketch.js
+++ 13-game-of-life/sketch.js
@@ -1,8 +1,9 @@
-// Game of life
+// Game of Life
 
 let grid;
 const GRID_SIZE = 30;
 let cellSize;
+let autoPlay = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,57 +19,68 @@ function setup() {
 
 function draw() {
   background(220);
-  displayGrid();
-}
-function keyTyped(){
-  if (key === "r"){
-    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-  }
-  else if (key === "e"){
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-  else if (key === " "){
+  if (autoPlay && frameCount % 10 === 0) {
     grid = nextTurn();
   }
+  displayGrid();
 }
 
-function nextTurn(){
+function keyTyped() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === " ") {
+    grid = nextTurn();
+  }
+  else if (key === "a") {
+    autoPlay = !autoPlay;
+  }
+}
+
+function nextTurn() {
   let nextTurnGrid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  for (let y=0;y<GRID_SIZE;y++){
-    for (let x=0;x<GRID_SIZE;x++){
-      // count neighbours
+
+  //look at every cell
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      //count neighbours
       let neighbours = 0;
 
-      // look at everything around me
-      for (let i = -1; i <=1; i++){
-        for (let j = -1; i <= 1; j++){
-          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE){
+      //look at all cells around in a 3x3 grid
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //detect edge cases
+          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE) {
             neighbours += grid[y+i][x+j];
           }
         }
       }
-
-      // checking about counting self
+      
+      //be careful about counting self
       neighbours -= grid[y][x];
 
-      // apply rules
-      if (grid[y][x] === 1){ // alive
-        if (neighbours === 2 || neighbours === 3){ // stay alive
+      //apply rules
+      if (grid[y][x] === 1) { //alive
+        if (neighbours === 2 || neighbours === 3) {
+          //stay alive
           nextTurnGrid[y][x] = 1;
         }
-        else { // die
+        else {
+          //died - lonely or overpopulation
           nextTurnGrid[y][x] = 0;
         }
       }
 
-      // dead
-      if (grid[y][x] === 0){
-        if (neighbours === 3){
-          // new birth
+      if (grid[y][x] === 0) { //dead
+        if (neighbours === 3) {
+          //new birth
           nextTurnGrid[y][x] = 1;
         }
         else {
-          // stay dead
+          //stay dead
           nextTurnGrid[y][x] = 0;
         }
       }
@@ -83,7 +95,6 @@ function mousePressed() {
 
   toggleCell(x, y);   //current cell
 }
-
 
 function toggleCell(x, y) {
   //check that we are within the grid, then toggle
