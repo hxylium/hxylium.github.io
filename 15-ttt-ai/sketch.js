81ca03b95596308ff4342c65a330101b9fe1b24e
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-11-06 12:16:59 -0600

    Started tic tac toe ai project

--- 15-ttt-ai/sketch.js
+++ 15-ttt-ai/sketch.js
@@ -3,7 +3,7 @@
 // 11/3/2023
 //
 // Extra for Experts:
-// - Utilized Minimaxer Algorithm/AI to be played against the user
+// - Utilized Minimaxer Algorithm/AI to be a player against the user
 
 
 
@@ -12,5 +12,16 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("white");
+  drawTicTacToeGrid(width/2, height/2);
+}
+
+function drawTicTacToeGrid(x, y){
+  let w = 400 / 3;
+  let h = 400 / 3;
+  strokeWeight(4);
+  line(x-w, 0, x-w, h*3);
+  line(x-(w * 2), 0, x-(w * 2), h*3);
+  line(x-w, h, w*3, h);
+  line(x-w, h * 2, w*3, h * 2);
 }
