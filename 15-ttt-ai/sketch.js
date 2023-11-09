188b978ae1bdc07fe9e1ad79e5b49b95faff0657
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-11-09 12:20:11 -0600

    Cooking

--- 15-ttt-ai/sketch.js
+++ 15-ttt-ai/sketch.js
@@ -2,10 +2,13 @@
 // Hyder Shahzaib Ahmed
 // 11/3/2023
 //
+// Resources: Coding Train Tutorial on Tic Tac Toe and Minimax
 // Extra for Experts:
 // - Utilized Minimaxer Algorithm/AI to be a player against the user
 
 const cellSize = 100;
+let Ypos = 200;
+let Xpos = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,17 +17,22 @@ function setup() {
 function draw() {
   background("white");
   drawGrid();
+  drawX();
 }
 
 function drawGrid() {
-  // draw the grid lines
   for (let y = 1; y < 3; y++) {
     for (let x = 1; x < 3; x++) {
       stroke("black");
-      strokeWeight(5);
-
-      line(x*cellSize, 0, x*cellSize, cellSize*3); // vertical lines
-      line(0, y*cellSize, cellSize*3 , y*cellSize); // horizontal lines
+      strokeWeight(6);
+      line(x*cellSize+Xpos, Ypos, x*cellSize+Xpos, cellSize*3+Ypos); // vertical lines
+      line(Xpos, y*cellSize+Ypos, cellSize*3+Xpos , y*cellSize+Ypos); // horizontal lines
     }
   }
+}
+
+function drawX(xpos, ypos){
+  textSize(60);
+  textFont("Arial");
+  text("X", xpos+cellSize/2-25+Xpos, 70+Ypos+ypos);
 }
